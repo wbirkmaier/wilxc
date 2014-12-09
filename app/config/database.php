f05d1dc3527f3c68bde2b8f4bfe152ea23654093
Wil Birkmaier <wil@birkmaier.org>
2014-12-09 01:35:31 -0500

    Added entries for wilxc database

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'laravel',
+			'database'  => 'wilxc',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
