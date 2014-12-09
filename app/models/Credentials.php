78d667d8959d8cd588c2b1a33bbd69fbcc49c1d1
Wil Birkmaier <wil@birkmaier.org>
2014-12-09 09:20:29 -0500

    Cleaned up Credentials with thought to be more than 1 in future per xenserver instance

--- app/models/Credentials.php
+++ app/models/Credentials.php
@@ -1,23 +1,37 @@
 <?php
+/*This */
 class Credentials extends Eloquent
     {   
-	public static function url()
+	private static function url()
 		{
 			$staticURL = "https://192.168.0.2";
 			return $staticURL;
 		}
 
-	public static function login()
-                {
+	private static function login()
+        {
 			$staticLogin = "root";
 			return $staticLogin;
-                }
+        }
 
-	public static function password()
-                {
-			$staticPassword = "";
+	private static function password()
+        {
+            $staticPassword = "";
 			return $staticPassword;
-                }
+        }
+    
+    public static function loginXen()
+        {
+            /*Pull credentials from private function in the Credentials class*/
+            $url = Credentials::url();
+            $login = Credentials::login();
+            $password = Credentials::password(); 
+
+            /*Connect to xenserver via HTTPS and create session*/
+            $xenserver = new XenApi($url, $login, $password);
+        
+            return $xenserver;
+        }
     }
 
 	
